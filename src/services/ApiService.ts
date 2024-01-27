37d7317889182d720caa2d4aec6f09d92e1e7ce4
Subham Surmara <anjalisurmara@Subhams-MacBook-Air.local>
2024-01-27 18:05:23 +0530

    subham: refactorfor login service

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -2,37 +2,38 @@
 import axios from 'axios';
 import mockApiData from '../MockData/mockApiData.json'
 
-const apiUrl = '/api'; // Adjust the API endpoint as needed
+const apiUrl = 'http://localhost:8080'; // Adjust the API endpoint as needed
 
 const ApiService = {
   login: async (email: string, password: string) => {
     // uncomment below code when actual service is created 
 
-    // try {
-    //   const response = await axios.post(`${apiUrl}/login`, { email, password });
-    //   return response.data;
-    // } catch (error) {
-    //   // Handle error (e.g., show error message)
-    //   console.error('Error during login:', error);
-    //   throw error;
-    // }
+    try {
+      debugger
+      const response = await axios.post(`${apiUrl}/login`, { username: email, password });
+      return response.data;
+    } catch (error) {
+      // Handle error (e.g., show error message)
+      console.error('Error during login:', error);
+      throw error;
+    }
 
     // comment below code when actual service is created 
 
-    try {
-        // Simulate delay to mimic network latency
-        await new Promise(resolve => setTimeout(resolve, 1000));
+    // try {
+    //     // Simulate delay to mimic network latency
+    //     await new Promise(resolve => setTimeout(resolve, 1000));
   
-        // Simulate API response based on email and password
-        const responseKey = email === '' && password === '' ? 'validUser' : 'invalidUser';
-        const responseData = mockApiData.login[responseKey];
+    //     // Simulate API response based on email and password
+    //     const responseKey = email === '' && password === '' ? 'validUser' : 'invalidUser';
+    //     const responseData = mockApiData.login[responseKey];
   
-        return responseData;
-      } catch (error) {
-        // Handle error (e.g., show error message)
-        console.error('Error during login:', error);
-        throw error;
-      }
+    //     return responseData;
+    //   } catch (error) {
+    //     // Handle error (e.g., show error message)
+    //     console.error('Error during login:', error);
+    //     throw error;
+    //   }
 
   },
 };
