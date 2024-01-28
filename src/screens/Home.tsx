f456d5a1e573f3a652806e82ec7b42d24987ca57
Subham Surmara <anjalisurmara@Subhams-MacBook-Air.local>
2024-01-28 09:14:01 +0530

    subham: added signup functionality

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,13 @@
 // screens/Home/Home.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import LoginForm from '../components/LoginForm/LoginForm';
-import '../styles/login-page.scss'; // Correct the import statement
+// import '../styles/login-page.scss'; // Correct the import statement
+import './Home.scss';
+import SignupForm from '../components/SignupForm/SignupForm';
 
 const Home: React.FC = () => {
+  const [showLogin, setShowLogin] = useState(true);
+  
   return (
     <div className="login-page">
       <div className="content">
@@ -25,7 +29,12 @@ const Home: React.FC = () => {
           Get started by signing in to your account and exploring the exciting features we offer!
         </p>
       </div>
-      <LoginForm />
+      <div className="form-container">
+        {showLogin ? <LoginForm /> : <SignupForm />}
+        <p onClick={() => setShowLogin(!showLogin)}>
+          {showLogin ? "Don't have an Account? Signup." : 'Already have an account? Login.'}
+        </p>
+      </div>
     </div>
   );
 };
