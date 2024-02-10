d0f536b20e757edc911e74def257b3ea8b3ae4ab
Subham Surmara <subhamsurmara@gmail.com>
2024-02-10 20:17:23 +0530

    subham: CSS

--- src/lib/cookie.ts
+++ src/lib/cookie.ts
@@ -31,9 +31,15 @@ export const useCustomCookie = (): CookieHook => {
     removeCookie(name);
   };
 
+
   const clear = () => {
-    const cookieKeys: string[] = cookies.getAll(); 
-    cookieKeys.forEach((key:string) => cookies.remove(key)); 
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i];
+        const eqPos = cookie.indexOf("=");
+        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
   };
 
   return {
