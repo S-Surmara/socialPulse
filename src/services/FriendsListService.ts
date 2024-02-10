5271244987e17a395ad1c41cc556cf2947914fbf
Subham Surmara <subhamsurmara@gmail.com>
2024-02-10 11:05:16 +0530

    subham : render friends posts

--- src/services/FriendsListService.ts
+++ src/services/FriendsListService.ts
@@ -1,8 +1,19 @@
 // PostService.ts
 import axios from 'axios';
 import friendsList from '../MockData/friendsList.json'
+const BASE_URL = 'http://localhost:8080/api/friendship';
 
 export const friendsService = {
+
+  getFrirndsList: async (userId: number) => {
+    try{
+      const response = await axios.get(`${BASE_URL}/${userId}/friends`);
+      return response
+    } catch {
+      console.log("error while fetching friends List");
+    }
+  },
+
   getFriendsListJson: () => {
     try {
       // For now, simulate fetching posts from a JSON file
